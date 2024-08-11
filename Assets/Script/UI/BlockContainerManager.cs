1bb9c41cb63729551c76584da7a47d3b7484cb85
eunjoo <quaziii9gamedev@gmail.com>
2024-08-11 22:16:13 +0900

    [feat] Block 위치에 따라 순서 재 정렬

--- Assets/Script/UI/BlockContainerManager.cs
+++ Assets/Script/UI/BlockContainerManager.cs
@@ -24,10 +24,50 @@ public class BlockContainerManager : Singleton<BlockContainerManager>
         BlockContainerBoxCollider.size = new Vector2(InventoryNum * 60, 60);
     }
 
-    public void AddBlock(GameObject gameObject)
+    public void AddBlock(GameObject newBlock)
     {
-        gameObject.transform.SetParent(this.transform);
+        // 현재 BlockContainer에 있는 모든 자식 블록들을 가져옵니다.
+        List<Transform> existingBlocks = new List<Transform>();
+        foreach (Transform child in this.transform)
+        {
+            existingBlocks.Add(child);
+        }
+
+        // 새로 추가될 블록의 월드 좌표
+        Vector3 newBlockWorldPosition = newBlock.transform.position;
+
+        // 모든 블록들의 월드 좌표를 비교하여 새로운 순서를 계산합니다.
+        List<Transform> sortedBlocks = new List<Transform>();
+
+        bool blockInserted = false;
+        foreach (Transform block in existingBlocks)
+        {
+            if (!blockInserted && newBlockWorldPosition.x < block.position.x)
+            {
+                sortedBlocks.Add(newBlock.transform);
+                blockInserted = true;
+            }
+            sortedBlocks.Add(block);
+        }
+
+        // 새 블록이 제일 마지막에 위치해야 할 경우
+        if (!blockInserted)
+        {
+            sortedBlocks.Add(newBlock.transform);
+        }
+
+        // 이제 블록들을 새로운 순서대로 배치합니다.
+        for (int i = 0; i < sortedBlocks.Count; i++)
+        {
+            sortedBlocks[i].SetParent(this.transform, false);
+            // Z 좌표를 0으로 설정
+            Vector3 newPosition = sortedBlocks[i].localPosition;
+            newPosition.z = 0;
+            sortedBlocks[i].localPosition = newPosition;
+            sortedBlocks[i].SetSiblingIndex(i);
+        }
     }
+
 
     public void RemoveCodeBlock(GameObject RemoveBlock)
     {
