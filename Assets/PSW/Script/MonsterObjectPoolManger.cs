d9271e04faeeab72216f96cd43fc46cc71d77352
박상욱 <vnlfltmxm@gmail.com>
2024-08-16 10:08:44 +0900

    [Add] 몬스터 프리팹 오브젝트 풀 추가

--- Assets/PSW/Script/MonsterObjectPoolManger.cs
+++ Assets/PSW/Script/MonsterObjectPoolManger.cs
@@ -8,13 +8,15 @@ public class MonsterObjectPoolManger : MonoBehaviour
     private List<GameObject> _monsterPrefabs = new List<GameObject>();
     [SerializeField]
     private int _poolCount;
-    private Dictionary<string, GameObject> _monsterPrefabsPool = new Dictionary<string, GameObject>();//->프리팹풀의 부모가 될녀석의 값을 참조하기위함
-    private Dictionary<string, Queue<GameObject>> _monster = new Dictionary<string, Queue<GameObject>>();//->꺼내쓸 풀
-
+    //private Dictionary<string, GameObject> _monsterPrefabsPool = new Dictionary<string, GameObject>();//->프리팹풀의 부모가 될녀석의 값을 참조하기위함
+    //private Dictionary<string, Queue<GameObject>> _monster = new Dictionary<string, Queue<GameObject>>();//->꺼내쓸 풀
+    private Dictionary<string, List<GameObject>> _monsterPrefabsPool = new Dictionary<string, List<GameObject>>();
+    private List<GameObject> _monsterPrefabObjList = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitPool();
+        SetMosterPool();
     }
 
     // Update is called once per frame
@@ -29,9 +31,7 @@ public class MonsterObjectPoolManger : MonoBehaviour
         {
             if (_monsterPrefabsPool.ContainsKey(_monsterPrefabs[i].name) == false)
             {
-                GameObject monsterPoolObj = new GameObject();
-                Instantiate(monsterPoolObj, this.gameObject.transform);
-                monsterPoolObj.name = _monsterPrefabs[i].name + "Pool";
+                _monsterPrefabsPool.Add(_monsterPrefabs[i].name, _monsterPrefabObjList);
 
             }
         }
@@ -42,31 +42,15 @@ public class MonsterObjectPoolManger : MonoBehaviour
         
         for (int i = 0; i < _monsterPrefabs.Count; i++)
         {
-            if (_monsterPrefabsPool.TryGetValue(_monsterPrefabs[i].name, out Dictionary<GameObject, Queue<GameObject>> innerDictionary))
-            {
-                GameObject gameObject = innerDictionary.Keys  GameObject;
-                // innerDictionary에서 특정 GameObject의 트랜스폼을 가져옵니다.
-                foreach (var gameObjectKey in innerDictionary.Keys)
-                {
-                    // GameObject의 Transform에 접근
-                    Transform objTransform = gameObjectKey.transform;
-
-                    // 원하는 작업 수행
-                    Debug.Log(objTransform.position);
-                }
-            }
-            else
-            {
-                Debug.LogWarning("해당 키에 대한 값이 없습니다.");
-            }
+            
 
             if (_monsterPrefabsPool.ContainsKey(_monsterPrefabs[i].name) == true)
             {
-                var gameObject = _monsterPrefabsPool[_monsterPrefabs[i].name].Keys;
 
                 for (int k = 0; k < _poolCount; k++)
                 {
-                    GameObject monsterPrefab = Instantiate(_monsterPrefabs[i], gameObject[_monsterPrefabs[i].name].);
+                    GameObject monsterPrefab = Instantiate(_monsterPrefabs[i], this.gameObject.transform);
+                    _monsterPrefabsPool[_monsterPrefabs[i].name].Add(monsterPrefab);
                     monsterPrefab.SetActive(false);
                 }
             }
@@ -78,7 +62,28 @@ public class MonsterObjectPoolManger : MonoBehaviour
     {
         if (_monsterPrefabsPool.Count > 0 && _monsterPrefabsPool.ContainsKey(monsterName) == true) 
         {
-            return _monsterPrefabsPool[monsterName].transform;
+            foreach (var item in _monsterPrefabsPool[monsterName])
+            {
+                if(item.activeSelf == false)
+                {
+                    return item;
+                }
+            }
+        }
+            return null;
+    }
+
+    public void DiableAllMonsterObjInList(string monsterName)
+    {
+        if ( _monsterPrefabsPool.ContainsKey(monsterName) == true)
+        {
+            foreach (var item in _monsterPrefabsPool[monsterName])
+            {
+                if (item.activeSelf == true)
+                {
+                    item.gameObject.SetActive(false);
+                }
+            }
         }
     }
 }
