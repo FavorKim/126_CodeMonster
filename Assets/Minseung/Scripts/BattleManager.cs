a0c0cd9cdbf88af04438c26960e4ef68b3656b42
김하랑 <kimharangs@gmail.com>
2024-08-20 14:39:24 +0900

    [docs] 잘못된 xml 및 엑셀 데이터 수정
    불 물 풀 -> 풀 물 불

--- Assets/Minseung/Scripts/BattleManager.cs
+++ Assets/Minseung/Scripts/BattleManager.cs
@@ -2,11 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BattleManager : MonoBehaviour 
+public class BattleManager : Singleton<BattleManager> 
 {
     //플레이어에서 넘어온 공격명령에 대한 배틀페이즈 실행.
 
-    public static BattleManager Instance { get; private set; }
 
     private DataManagerTest dataManager;
     private Player player;
@@ -21,21 +20,15 @@ public class BattleManager : MonoBehaviour
 
     public void BattlePhase(Vector2Int playerPosition, int attackBlockType)
     {
-        
 
+
+        DebugBoxManager.Instance.Log($"{dataManager.GetMonsterTypeData(attackBlockType).TypeViewName} Type Attack");
         // 플레이어의 위치와 적의 위치를 비교하여 같은 위치에 있는지 확인
 
         if (stageManager.CheckMonsterAndPlayerPos(playerPosition))
         {
             GameObject monsterObj= stageManager.GetMonsterWithPlayerPos(playerPosition);
-            if(monsterObj == null )
-            {
-                // 적의 위치와 일치하지 않는 경우, 공격이 빗나간 것으로 간주하여 패배 처리
-                //-> 빗나가면 아무일도 없으니 승리 처리로 바꿈
-                Debug.Log("Attack missed!");
-                StageManager.Instance.GetPlayer().Win();
-                return;
-            }
+           
             string monsterName = monsterObj.name;
             Monster monster = DataManagerTest.Instance.GetMonsterData(monsterName);
             MonsterType monsterType = DataManagerTest.Instance.GetMonsterTypeData(monster.TypeIndex);
@@ -45,6 +38,7 @@ public class BattleManager : MonoBehaviour
             {
                 Debug.Log("Attack successful! Monster defeated.");
                 Debug.Log(dataManager.GetMonsterTypeData(attackBlockType).TypeViewName + "으로 공격함");
+                DebugBoxManager.Instance.Log($"{dataManager.GetMonsterTypeData(attackBlockType).TypeViewName} Type Attack");
                 // 승리 처리: 플레이어의 승리 메서드 호출
                 StartCoroutine(TestTimer());
                 //-> 플레이어의 공격 애니메이션과 이펙트가 끝나면
@@ -60,6 +54,13 @@ public class BattleManager : MonoBehaviour
                 StartCoroutine(TestTimer());//->적의 애니매이션 실행부분
                 StageManager.Instance.GetPlayer().Defeat();
             }
+        }
+        else
+        {
+            // 적의 위치와 일치하지 않는 경우, 공격이 빗나간 것으로 간주하여 패배 처리
+            //-> 빗나가면 아무일도 없으니 승리 처리로 바꿈
+            Debug.Log("Attack missed!");
+            StageManager.Instance.GetPlayer().Win();
         }
 
 
