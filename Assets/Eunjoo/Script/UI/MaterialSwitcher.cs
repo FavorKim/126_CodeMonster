09fe72ee0a37c149fc0af8d433eb228a54d24954
Eunjoo <quaziii9gamedev@gmail.com>
2024-08-20 14:05:57 +0900

    [feat] 코드블럭 프리팹에 마테리얼 변경 세팅

--- Assets/Eunjoo/Script/UI/MaterialSwitcher.cs
+++ Assets/Eunjoo/Script/UI/MaterialSwitcher.cs
@@ -1,43 +1,53 @@
 using UnityEngine;
 
+
+
+
 public class MaterialChanger : MonoBehaviour
 {
     public Material[] materials;  // 3개의 머티리얼 배열로 설정
 
     private Renderer rend;  // 오브젝트의 렌더러
 
-    void Start()
+    private void Start()
     {
         rend = GetComponent<Renderer>();
-
-        // 기본적으로 첫 번째 머티리얼을 할당
-        if (materials.Length > 0)
-        {
-            rend.material = materials[0];
-        }
     }
 
-    void Update()
+    private void Update()
+    {
+        keyDown();
+    }
+
+    private void keyDown()
     {
         if (Input.GetKeyDown(KeyCode.Alpha0))  // 키보드 '0' 키를 눌렀을 때
         {
-            ChangeMaterial(0);  // 첫 번째 머티리얼로 변경
+            ChangeMaterial(MaterialType.NORMAL_CODEBLOCK_MATERIAL);  // 첫 번째 머티리얼로 변경 : 일반 
         }
         else if (Input.GetKeyDown(KeyCode.Alpha1))  // 키보드 '1' 키를 눌렀을 때
         {
-            ChangeMaterial(1);  // 두 번째 머티리얼로 변경
+            ChangeMaterial(MaterialType.OUTLINE_CODEBLOCK_MATERIAL);  // 두 번째 머티리얼로 변경 : 
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))  // 키보드 '2' 키를 눌렀을 때
         {
-            ChangeMaterial(2);  // 세 번째 머티리얼로 변경
+            ChangeMaterial(MaterialType.USE_CODEBLOCK_MATERIAL);  // 세 번째 머티리얼로 변경
         }
     }
 
-    void ChangeMaterial(int index)
+    private void ChangeMaterial(int index)
     {
         if (index >= 0 && index < materials.Length)
         {
             rend.material = materials[index];  // 인덱스에 해당하는 머티리얼로 변경
         }
     }
+}
+
+
+public static class MaterialType
+{
+    public const int NORMAL_CODEBLOCK_MATERIAL = 0;
+    public const int OUTLINE_CODEBLOCK_MATERIAL = 1;
+    public const int USE_CODEBLOCK_MATERIAL = 2;
 }
