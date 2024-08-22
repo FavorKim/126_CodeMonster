081ada98051e43649809c0fc7ab2b1bdca9e6a01
Eunjoo <quaziii9gamedev@gmail.com>
2024-08-22 10:40:25 +0900

    [feat] Outline 다른 방법으로 수정

--- Assets/Eunjoo/Script/UI/MaterialSwitcher.cs
+++ Assets/Eunjoo/Script/UI/MaterialSwitcher.cs
@@ -1,3 +1,4 @@
+using EPOOutline;
 using UnityEngine;
 
 
@@ -8,10 +9,12 @@ public class MaterialChanger : MonoBehaviour
     public Material[] materials;  // 3개의 머티리얼 배열로 설정
 
     private Renderer rend;  // 오브젝트의 렌더러
-
-    
-    
-
+    private Outlinable outlineable;
+    private void Start()
+    {
+        outlineable = gameObject.GetComponentInParent<Outlinable>();
+        outlineable.AddAllChildRenderersToRenderingList();
+    }
 
     public void ChangeMaterial(MaterialType index)
     {
@@ -21,6 +24,23 @@ public class MaterialChanger : MonoBehaviour
                 rend = GetComponent<Renderer>();
             rend.material = materials[(int)index];  // 인덱스에 해당하는 머티리얼로 변경
         }
+        if ((int)index == 0) ChangeOutlineNormal();
+        else if((int)index == 1) ChangeOutlinePlay();
+        else if ((int)index == 2) ChangeOutlineUsed();
+    }
+
+    public void ChangeOutlineNormal()
+    {
+        outlineable.OutlineParameters.Color = Color.white;
+    }
+
+    public void ChangeOutlinePlay()
+    {
+        outlineable.OutlineParameters.Color = Color.red;
+    }
+    public void ChangeOutlineUsed()
+    {
+        outlineable.OutlineParameters.Color = Color.black;
     }
 }
 
