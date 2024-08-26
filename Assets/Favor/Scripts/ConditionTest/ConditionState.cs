f309ad766bc85a1783ff4e61d6eb83bbaab8ab93
김하랑 <kimharangs@gmail.com>
2024-08-26 17:56:32 +0900

    [feat] 조건블록 예시 수정

--- Assets/Favor/Scripts/ConditionTest/ConditionState.cs
+++ Assets/Favor/Scripts/ConditionTest/ConditionState.cs
@@ -18,11 +18,12 @@ namespace FavorSample
         public SampleStateMachine StateMachine { get; set; }
     }
 
+    // 상태머신
     public class SampleStateMachine
     {
         SamplePlayer player;
         SampleBaseState<SamplePlayer> curState;
-        Dictionary<Enum, SampleBaseState<SamplePlayer>> dict;
+        Dictionary<Enum, SampleBaseState<SamplePlayer>> dict = new Dictionary<Enum, SampleBaseState<SamplePlayer>>();
 
         // 상태머신 초기화
         public SampleStateMachine(SamplePlayer player)
@@ -41,15 +42,20 @@ namespace FavorSample
             }
         }
 
-        // 상태 변화 함수. 조건 상태가 아닐 경우 조건상태에 필요한 변수는 default 매개변수로 선언
-        public void ChangeState(Enum state, bool cond = true, Action act = null)
+        // 상태 변화 함수. 조건 상태가 아닐 경우 조건상태에 필요한 조건 블록 변수는 default 매개변수로 선언
+        public void ChangeState(Enum state, ConditionCodeBlock block = null)
         {
             SampleBaseState<SamplePlayer> next;
             // 만약 조건 상태로 변경할 경우
-            if (state is SampleStates.Condition)
+            if (state is SampleStates.Condition && block != null)
             {
                 // 조건을 초기화
-                (dict[state] as ConditionState).InitCondition(cond, act);
+                (dict[state] as ConditionState).InitCondition(block.condition, block.act);
+            }
+            // 코드블록을 넣어주지 않았다면
+            else
+            {
+                // 에러
             }
             next = dict[state];
 
@@ -60,7 +66,7 @@ namespace FavorSample
 
 
     }
-    
+
 
     // 조건상태
     public class ConditionState : SampleBaseState<SamplePlayer>
@@ -71,11 +77,11 @@ namespace FavorSample
             owner = player;
         }
 
-        bool cond;
+        Func<bool> cond;
         Action act;
 
         // 조건 초기화 함수
-        public void InitCondition(bool cond, Action act)
+        public void InitCondition(Func<bool> cond, Action act)
         {
             this.cond = cond;
             this.act = act;
@@ -92,27 +98,33 @@ namespace FavorSample
         private void ConditionAction()
         {
             // 조건에 부합하면 실행
-            if (cond == true)
+            if (cond.Invoke() == true)
                 act.Invoke();
 
         }
     }
 
     // 체크 상태
-    public class CheckState : SampleBaseState<SamplePlayer> 
+    public class CheckState : SampleBaseState<SamplePlayer>
     {
         SamplePlayer owner;
         public CheckState(SamplePlayer player) : base(player) { owner = player; }
 
         // 받아올 코드블럭 리스트
-        List<int> blocks = new();
+        List<int> blocks = new List<int>();
 
+        // 현재 
         int listCount = 0;
         public override void OnEnterState()
         {
             // 코드블럭 세팅
-            blocks.Clear();
-            blocks = BlockContainerManager.Instance.GetContatinerBlocks();
+            List<int> newBlocks = BlockContainerManager.Instance.GetContatinerBlocks();
+            // 블록리스트가 달라지면 (새로 코드를 짜서 실행하면) 리스트 카운트 초기화
+            if (blocks != newBlocks)
+                listCount = 0;
+
+            blocks = newBlocks;
+
             switch (blocks[listCount])
             {
                 case 0:
@@ -134,7 +146,7 @@ namespace FavorSample
             }
         }
 
-       
+
         public void IncreaseListCount()
         {
             listCount++;
@@ -168,7 +180,17 @@ namespace FavorSample
 
     }
 
-    
+
+    public class ConditionCodeBlock
+    {
+        public Func<bool> condition { get; private set; }
+        public Action act { get; private set; }
+        public ConditionCodeBlock(Func<bool> condition, Action act)
+        {
+            this.condition = condition;
+            this.act = act;
+        }
+    }
 
     // 플레이어의 상태를 변경하기위해 선언한 클래스
     public class SampleStateChanger
@@ -177,12 +199,13 @@ namespace FavorSample
         public void ConditionTest(Vector2Int pos)
         {
             // 조건을 설정 (아래와 같이 기존에 있던 함수를 활용할 예정)
-            bool cond = StageManager.Instance.CheckMonsterAndPlayerPos(pos);
+            Func<bool> cond = () => StageManager.Instance.CheckMonsterAndPlayerPos(pos);
             // 조건 만족시 수행할 행동을 설정
             Action act = ChangeStateToBattle;
+            var block = new ConditionCodeBlock(cond, act);
 
-            // 조건 수립 후 상태 변경
-            player.StateMachine.ChangeState(SampleStates.Condition, cond, act);
+            // 조건 설정 후 상태 변경
+            player.StateMachine.ChangeState(SampleStates.Condition, block);
         }
         public void ChangeStateToBattle()
         {
@@ -192,14 +215,14 @@ namespace FavorSample
     }
 
 
-    
+
     public abstract class SampleBaseState<T> where T : class
     {
         protected T Controller { get; set; }
 
         // 행동 종료시 호출할 이벤트
         public event Action OnActionEnd;
-        
+
         // 이벤트 핸들러 호출 함수
         public void InvokeOnActionEnd()
         {
