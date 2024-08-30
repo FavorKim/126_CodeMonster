3e894964bcc41ca606775f9a779f7b391ece0fd1
김하랑 <kimharangs@gmail.com>
2024-08-30 17:39:37 +0900

    [feat] 포크 드랍다운 기능 구현 완료. 그러나 범위가 너무 작음

--- Assets/Favor/Scripts/ConditionTest/PokeDropdown.cs
+++ Assets/Favor/Scripts/ConditionTest/PokeDropdown.cs
@@ -1,25 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PokeDropdown : MonoBehaviour
 {
-    Dropdown dropDown;
+    [SerializeField] TMP_Dropdown dropDown;
     CustomPokedObject poke;
 
     private void Awake()
     {
-        dropDown = GetComponent<Dropdown>();
         poke = GetComponent<CustomPokedObject>();
     }
 
-    private void OnEnable()
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            dropDown.Show();
+        }
+    }
+    private void Start()
     {
         poke.OnPoke += dropDown.Show;
     }
-    private void OnDisable()
+    private void OnApplicationQuit()
     {
         poke.OnPoke -= dropDown.Show;
     }
+
+    public void OnPokeItem(Toggle toggle)
+    {
+        if (!toggle.isOn)
+        {
+            toggle.isOn = true;
+        }
+
+        int num = -1;
+        Transform transform = toggle.transform;
+        Transform parent = transform.parent;
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            if (parent.GetChild(i) == transform)
+            {
+                num = i - 1;
+                break;
+            }
+        }
+
+        if (num >= 0)
+        {
+            dropDown.value = num;
+            dropDown.Hide();
+        }
+    }
+
 }
