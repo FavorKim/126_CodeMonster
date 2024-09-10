da4ef77663b95d5d8f583ce87f70551da816ea4a
Eunjoo <quaziii9gamedev@gmail.com>
2024-09-10 15:10:33 +0900

    [feat] 챕터 클릭시 저장 함수 설정

--- Assets/Eunjoo/Script/OutgameUIManager.cs
+++ Assets/Eunjoo/Script/OutgameUIManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class OutgameUIManager : MonoBehaviour
@@ -7,7 +8,10 @@ public class OutgameUIManager : MonoBehaviour
     [SerializeField] GameObject IntroUI;
     [SerializeField] GameObject ChapterUI;
     [SerializeField] GameObject StageUI;
-    
+    [SerializeField] TextMeshProUGUI ChapterText; // 챕터 텍스트 UI
+
+    string chapterNumber = ""; // 챕터 번호 저장 변수
+
     public void ClickStartButton()
     {
         IntroUI.SetActive(false);
@@ -24,6 +28,9 @@ public class OutgameUIManager : MonoBehaviour
     {
         ChapterUI.SetActive(false);
         StageUI.SetActive(true);
+
+        // 저장된 챕터 번호를 텍스트로 표시
+        ChapterText.text = "챕터 " + chapterNumber;
     }
 
     public void ClickGoToChapter()
@@ -35,5 +42,24 @@ public class OutgameUIManager : MonoBehaviour
     public void ClickStage()
     {
         StageUI.SetActive(false);
+    }
+
+    // 챕터 버튼 클릭 시 호출되는 함수
+    public void OnChapterClick(GameObject clickedObject)
+    {
+        // 클릭할 때마다 chapterNumber를 초기화하여 중복 방지
+        chapterNumber = "";
+
+        // 오브젝트 이름에서 숫자를 추출 (예: "CHAPTER1")
+        string objectName = clickedObject.name; // 예: "CHAPTER1", "CHAPTER2"
+
+        // 이름에서 숫자만 추출 (숫자라면 chapterNumber에 저장)
+        foreach (char c in objectName)
+        {
+            if (char.IsDigit(c))
+            {
+                chapterNumber += c;  // 숫자를 chapterNumber에 추가
+            }
+        }
     }
 }
