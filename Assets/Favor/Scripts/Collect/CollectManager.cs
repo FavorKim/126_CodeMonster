26ea76da7bd91c636018ff5e9a821434095979e0
김하랑 <kimharangs@gmail.com>
2024-09-26 09:41:20 +0900

    [bugfix] 수집씬에서 몬스터가 제대로 움직이지 않았던 버그 수정

--- Assets/Favor/Scripts/Collect/CollectManager.cs
+++ Assets/Favor/Scripts/Collect/CollectManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -132,7 +133,7 @@ public class CollectManager : Singleton<CollectManager>
             }
             else
             {
-                spawnedMonster.transform.position = Vector3.Lerp(FeedArea.position, MonsterSpawnPos.position, CaptureGauge / MaxCaptureGauge);
+                spawnedMonster.transform.position = Vector3.Lerp(FeedArea.position, MonsterSpawnPos.position, 1 - (CaptureGauge / MaxCaptureGauge));
             }
         }
     }
